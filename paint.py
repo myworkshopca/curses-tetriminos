2381764bf89203501b55480aa1b1d26c94c31a6a
Sean Chen <Sean.Chen@leocorn.com>
2021-05-02 18:33:39 -0400

    add z shape tetriminos

--- paint.py
+++ paint.py
@@ -53,6 +53,18 @@ def paintTetrimino(stdscr, color, type):
         stdscr.addstr(17, 2, unit_ch, color)
         stdscr.addstr(17, 4, unit_ch, color)
         stdscr.addstr(17, 6, unit_ch, color)
+    elif type == 'z':
+        # l shape tetrimino
+        stdscr.addstr(19, 2, unit_ch, color)
+        stdscr.addstr(19, 4, unit_ch, color)
+        stdscr.addstr(20, 4, unit_ch, color)
+        stdscr.addstr(20, 6, unit_ch, color)
+    elif type == 'zr':
+        # l shape tetrimino
+        stdscr.addstr(22, 4, unit_ch, color)
+        stdscr.addstr(22, 6, unit_ch, color)
+        stdscr.addstr(23, 4, unit_ch, color)
+        stdscr.addstr(23, 2, unit_ch, color)
 
 def tetris(stdscr):
 
@@ -68,7 +80,10 @@ def tetris(stdscr):
     paintTetrimino(stdscr, curses.color_pair(5), 'line')
     paintTetrimino(stdscr, curses.color_pair(6), 't')
     paintTetrimino(stdscr, curses.color_pair(7), 'l')
-    paintTetrimino(stdscr, curses.color_pair(8), 'lr')
+    # 8 is white, 9 is grey, we want some colorful thing...
+    paintTetrimino(stdscr, curses.color_pair(10), 'lr')
+    paintTetrimino(stdscr, curses.color_pair(11), 'z')
+    paintTetrimino(stdscr, curses.color_pair(12), 'zr')
 
     stdscr.getch()
 
